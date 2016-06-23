87e00db028730ee2366ad4d4733972d1e0ea3108
Vasil <v.mitov.clouway@gmail.com>
2016-06-23 14:11:19 +0300

    Made some refractoring to the SmSender class.

--- src/SmSender.java
+++ src/SmSender.java
@@ -6,28 +6,42 @@ import java.util.regex.Pattern;
 public class SmSender implements Validator {
   @Override
   public boolean checkContentOf(Sms sms) {
+    if (titleChecker(sms)||contextChecker(sms)||numberChecker(sms)){
+      return false;
+    }
+    return true;
+  }
+
+  private boolean titleChecker(Sms sms) {
     if (sms.getTitle().isEmpty()) {
-      return false;
+      return true;
     }
-    if (sms.getContext().isEmpty()) {
-      return false;
-    }
-    if(sms.getContext().length()<10||sms.getContext().length()>100){
-      return false;
-    }
+    return false;
+  }
+
+  private boolean numberChecker(Sms sms) {
     if (sms.getNumberAsString().isEmpty())
-      return false;
+      return true;
     if (!sms.getNumberAsString().startsWith("08")) {
-      return false;
+      return true;
     }
     if (!sms.getNumberAsString().matches("[0-9]+")){
-      return false;
+      return true;
     }
     if (sms.getNumberAsString().length() < 10 || sms.getNumberAsString().length() > 10) {
-      return false;
+      return true;
     }
+    return false;
+  }
 
-    return true;
+  private boolean contextChecker(Sms sms) {
+    if (sms.getContext().isEmpty()) {
+      return true;
+    }
+    if(sms.getContext().length()<10||sms.getContext().length()>100){
+      return true;
+    }
+    return false;
   }
 
 }
